1cd3ed970e164c3d05a08b34fe863d3a02cbd2bd
wojteksyk <144209174+wojteksyk@users.noreply.github.com>
2025-05-26 13:05:21 +0200

    Delete project_AW directory

--- project_AW/backend/src/models/Song.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const songSchema = new mongoose.Schema({
-    title: String,
-    artist: String,
-    rating: Number
-});
-
-module.exports = mongoose.model('Song', songSchema);
